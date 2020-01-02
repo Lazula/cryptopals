cafe3c67650261e52cef1ad4336f98506de4b4cb
Lazula <26179473+Lazula@users.noreply.github.com>
2020-01-02 15:44:47 -0600

    Fixed heap overread in 09

--- Set 2/09. Implement PKCS#7 padding/main.c
+++ Set 2/09. Implement PKCS#7 padding/main.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "../../include/crypto_utility.h"
 
 int main(){
 	char *input = "YELLOW SUBMARINE";
-	unsigned char *padded = NULL;
-	
+	unsigned char *padded_data = NULL;
+	char *padded_string = NULL;
 	const size_t block_size = 20;
+	size_t padded_data_size;
+	size_t padded_string_size;
 		
-	pkcs7_pad(&padded, (unsigned char *) input, 16, block_size);
-	printf("%s\n", padded);
+	padded_data_size = pkcs7_pad(&padded_data, input, strlen(input), block_size);
+	padded_string_size = padded_data_size+1;
+	padded_string = malloc(padded_string_size);
+
+	memcpy(padded_string, padded_data, padded_data_size);
+	padded_string[padded_string_size-1] = '\0';
 	
-	free(padded);
+	printf("%s\n", padded_string);
+	
+	free(padded_data);
+	free(padded_string);
 }
