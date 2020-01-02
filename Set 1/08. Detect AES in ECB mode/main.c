ce85d2407fafdb1ce6d8755201a4b4bd6feafb02
Lazula <26179473+Lazula@users.noreply.github.com>
2020-01-02 15:36:15 -0600

    Updated and cleaned up 08

--- Set 1/08. Detect AES in ECB mode/main.c
+++ Set 1/08. Detect AES in ECB mode/main.c
@@ -11,31 +11,36 @@ int main(int argc, char *argv[]){
 	FILE *const data_file = fopen("data.txt", "r");
 	//line_buffer accepts up to 1MB-1
 	size_t line_buffer_size = 1048576;
-	unsigned char *const line_buffer = calloc(line_buffer_size, sizeof(unsigned char)), *linebreak, *raw_encrypted_data, *decrypted_data;
+	char *line_buffer = malloc(line_buffer_size);
+	char *linebreak = NULL;
+	unsigned char *raw_encrypted_data = NULL;
+	unsigned char *decrypted_data = NULL;
 	
-	while(getdelim((char **)&line_buffer, &line_buffer_size, '\n', data_file) > -1){
+	while(getdelim(&line_buffer, &line_buffer_size, '\n', data_file) > -1){
 		linebreak = strchr(line_buffer, '\n');
 		if(linebreak != NULL) *linebreak = '\0';
+		raw_encrypted_data = NULL;
 		const size_t raw_data_size = hex_decode(&raw_encrypted_data, line_buffer);
 		unsigned char found_ecb_128 = is_aes_ecb(raw_encrypted_data, raw_data_size, AES_KEY_128);
 		if(found_ecb_128){
-			printf("Found potential AES-128-ECB: %s", line_buffer);
+			printf("Found potential AES-128-ECB: %s\n", line_buffer);
 		}
 		
 		unsigned char found_ecb_192 = is_aes_ecb(raw_encrypted_data, raw_data_size, AES_KEY_192);
 		if(found_ecb_192){
-			printf("Found potential AES-192-ECB: %s", line_buffer);
+			printf("Found potential AES-192-ECB: %s\n", line_buffer);
 		}
 		
 		unsigned char found_ecb_256 = is_aes_ecb(raw_encrypted_data, raw_data_size, AES_KEY_256);
 		if(found_ecb_256){
-			printf("Found potential AES-256-ECB: %s", line_buffer);
+			printf("Found potential AES-256-ECB: %s\n", line_buffer);
 		}
+		
+		free(raw_encrypted_data);
 	}
 	
 	fclose(data_file);
 	free(line_buffer);
-	free(raw_encrypted_data);
 }
 
 unsigned char is_aes_ecb(unsigned char *data, size_t data_size, uint8_t key_type){
